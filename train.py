f9ed29c3ba8f98b46eabc97eafef82790d6be5c4
Krypto12 <35391676+Krypto12@users.noreply.github.com>
2018-12-14 13:44:25 +0000

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,5 @@
 #from model import neuralnet
-import numpy as np
 
-import csv
-import os
 # image_data,y,image_test,text_y=tflearn.datasets.mnist.load_data(one_hot=True)
 #
 # image_input=image_data.reshape([-1,28,28,1])
@@ -70,13 +67,66 @@ def process_data():
 
 def bitcoin():
     import pandas
+    import tflearn
+    import numpy as np
+    import cv2
+    from tflearn.data_utils import to_categorical, pad_sequences
+    from tflearn.datasets import imdb
+    import tensorflow as tf
+
 
     data=pandas.read_csv("bitstampUSD_1-min_data_2012-01-01_to_2018-11-11.csv")
     df = pandas.DataFrame(data)
     train=data[:int(len(data)/2)].to_dict()
-    test=data[int(len(data)/2):]
-    trainY,trainX=train["Timestamp"],test["Weighted_Price"]
-    print(train["Weighted_Price"])
+    test=data[int(len(data)/2):].to_dict()
+    trainY,trainX=list(train["Timestamp"].values()),list(train["Weighted_Price"].values())
+    testY,testX=list(test["Timestamp"].values()),list(test["Weighted_Price"].values())
     #time stamp is y val and weighted price is x
+
+    # Converting labels to binary vectors
+    # trainY = to_categorical(trainY,10000000000000000000000000000000)
+    # testY = to_categorical(testY,1000000000000000000000000000000000)
+    def chunks(l, n):
+        """Yield successive n-sized chunks from l."""
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+    # testX=[x if (x!="NaN") else None for x in testX]
+    # new_testx=[]
+    # new_testy=[]
+    # new_trainx = []
+    # new_trainy = []
+    # testx=chunks(testX,100)
+    # for i in testx:
+    #     new_testx.append(i)
+    # testy=chunks(testY,100)
+    # for j in testy:
+    #     new_testy.append(j)
+    # trainx=chunks(trainX,100)
+    # trainy=chunks(trainY,100)
+    # for k in trainx:
+    #     print(k)
+    #     new_trainx.append(k)
+    # for l in trainy:
+    #     new_trainy.append(l)
+    #
+    # np.reshape(trainX,[-1,100,1])
+    # np.reshape(trainY, [-1, 100, 1])
+    # np.reshape(testX, [-1, 100, 1])
+    # np.reshape(testY, [-1, 100, 1])
+    # print(new_trainx[:5])
+    # print(new_trainy[:5])
+    #print([x for x in new_trainx])
+    net = tflearn.input_data([None,len(trainX)])
+    net = tflearn.embedding(net, input_dim=10000, output_dim=128)
+    net = tflearn.lstm(net, 128, dropout=0.8)
+    net = tflearn.fully_connected(net, 2, activation='softmax')
+    net = tflearn.regression(net, optimizer='adam', learning_rate=0.001,
+                             loss='categorical_crossentropy')
+
+    # Training
+    model = tflearn.DNN(net, tensorboard_verbose=0)
+
+#        model.fit(np.array(new_trainx)[i], np.array(new_trainy)[i], validation_set=(np.array(new_testx)[i], np.array(new_testy)[i]), show_metric=True,batch_size=100)
+    model.fit(np.array(trainX), np.array(trainY), validation_set=(np.array(testX), np.array(testY)), show_metric=True,batch_size=100)
 
 bitcoin()
